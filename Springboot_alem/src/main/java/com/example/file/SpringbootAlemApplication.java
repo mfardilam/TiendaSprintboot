717bcec3ce9a5e7d8a793e1f1613bb750d12d533
llANDREZll <andresbenpin.10@gmail.com>
2021-10-08 08:19:49 -0500

    Comandos base de datos

--- Springboot_alem/src/main/java/com/example/file/SpringbootAlemApplication.java
+++ Springboot_alem/src/main/java/com/example/file/SpringbootAlemApplication.java
@@ -11,11 +11,13 @@ import com.example.file.controller.CSVController;
 
 INSERT INTO authority (id,authority) VALUES (1,"ROLE_ADMIN");
 
+INSERT INTO usuarios (id,enabled,password,username) 
+VALUES(1,0b1,"$2a$04$rtiGhEOfRS7ZfHFL2a1I5eEcYtjwo.5sq1hYgIFu9W0s0BS3gxwWy","admin");
+
 INSERT INTO authorities_users (usuario_id, authority_id) VALUES (1,1);
 INSERT INTO authorities_users (usuario_id, authority_id) VALUES (1,2);
 
-INSERT INTO usuarios (id,enabled,password,username) 
-VALUES(1,0b1,"$2a$04$rtiGhEOfRS7ZfHFL2a1I5eEcYtjwo.5sq1hYgIFu9W0s0BS3gxwWy","admin");
+
 
  */
 
