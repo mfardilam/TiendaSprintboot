45ee15958211389fe873eaca1cdd4d175de17bf9
cristian1767 <90486186+cristian1767@users.noreply.github.com>
2021-10-16 21:27:36 -0500

    prueba

--- Springboot_alem/src/main/java/com/example/file/model/Factura.java
+++ Springboot_alem/src/main/java/com/example/file/model/Factura.java
@@ -21,7 +21,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-
+import javax.validation.constraints.*;
 
 @Entity
 @Table(name = "facturas")
@@ -31,7 +31,7 @@ public class Factura implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	
+	@NotEmpty
 	private String descripcion;
 
 	private String observacion;
